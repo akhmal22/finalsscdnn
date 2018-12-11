d84f508e31528bfbc21a8db0a897a3da3fe1b29c
akhmal22 <rizkyantorig355@gmail.com>
2018-12-12 03:40:48 +0700

    test dataset preps

---
+++ scratch/addResize.py
@@ -0,0 +1,38 @@
+import time
+import numpy as np
+import h5py
+import scipy
+from PIL import Image
+from pathlib import Path
+import cv2
+
+def make_square(path_to_img):
+    '''
+        thanks to stephen rauch, jdhao
+    '''
+    desired_size = 64
+    im_pth = path_to_img
+    im = Image.open(im_pth)
+    old_size = im.size  # old_size[0] is in (width, height) format
+    ratio = float(desired_size)/max(old_size)
+    new_size = tuple([int(x*ratio) for x in old_size])
+    # use thumbnail() or resize() method to resize the input image
+    # thumbnail is a in-place operation
+    # im.thumbnail(new_size, Image.ANTIALIAS)
+    im = im.resize(new_size, Image.ANTIALIAS)
+    # create a new image and paste the resized on it
+    new_im = Image.new("RGB", (desired_size, desired_size))
+    new_im.paste(im, ((desired_size-new_size[0])//2,
+                        (desired_size-new_size[1])//2))
+    return new_im
+
+for itr in range(1,52):
+    if Path("train//{}.jpg".format(str(itr).zfill(5))).is_file():
+        imsq = make_square("train//{}.jpg".format(str(itr).zfill(5)))
+        imsq.save("train//square{}.jpg".format(str(itr)))
+    
+
+for itr in range(1,23):
+    if Path("test//{}.jpg".format(str(itr))).is_file():
+        imsq = make_square("test//{}.jpg".format(str(itr)))
+        imsq.save("test//square{}.jpg".format(str(itr)))
